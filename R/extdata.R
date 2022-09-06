6782f6abfa1d3b2711fd332043d85e5947091f9c
Aaron Willcox <awillcox@deakin.edu.au>
2022-09-06 16:05:47 +1000

    - Moved bash files to `extdata`.

---
+++ R/extdata.R
@@ -0,0 +1,32 @@
+#' Install Script
+#'
+#' This is a dataset containing stuff.
+#'
+#' @name install_additional
+#'
+#' @section install_additional.sh:
+#'
+#' This data is used in write_install_bash_file().
+NULL
+
+#' Install Script
+#'
+#' This is a dataset containing stuff.
+#'
+#' @name install_pyenv
+#'
+#' @section install_pyenv.sh:
+#'
+#' This data is used in write_install_bash_file().
+NULL
+
+#' Install Script
+#'
+#' This is a dataset containing stuff.
+#'
+#' @name install_python
+#'
+#' @section install_python.sh:
+#'
+#' This data is used in write_install_bash_file().
+NULL
