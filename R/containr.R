68451e3c60369fffc556d17ea46f54379ff65f7a
Aaron Willcox <awillcox@deakin.edu.au>
2022-09-20 12:01:33 +1000

    - testing subclass `dockerfile` to super `containr`.

--- R/containr.R
+++ R/containr.R
@@ -20,6 +20,8 @@ containr <- R6::R6Class("containr",
     #' @field name Name of the containr when launched. Defaults to the active Rstudio Project.
     name = NULL,
 
+    tag = NULL,
+
     #' Start a ContainR
     #'
     #' @description
@@ -39,7 +41,7 @@ containr <- R6::R6Class("containr",
     #'
     #' @param use_local If you want to port your local Rstudio session into the containr.
     #' Defaults to `TRUE`.
-    initialize = function(image = NA, name = NA, DISABLE_AUTH = TRUE, use_local = TRUE){
+    initialize = function(image = NULL, name = NULL, DISABLE_AUTH = TRUE, use_local = TRUE){
 
       self$set_image(image)
       self$set_name(name)
@@ -55,11 +57,19 @@ containr <- R6::R6Class("containr",
     set_image = function(image){
       if(is.null(image)){
         self$image <- "rstudio"
+      } else if(image %in% data_rocker_table$name) {
+        # Switch for changing rocker image based on image name
+        self$image <- switch(image,
+          rstudio = paste0("rocker/rstudio:", self$tag),
+          tidyerse = paste0("rocker/tidyverse:", self$tag),
+          verse = paste0("rocker/verse:", self$tag),
+          geospatial = paste0("rocker/geospatial:", self$tag),
+          binder = paste0("rocker/binder:", self$tag))
       } else {
-        self$image <- image
+        private$proj_image <- self$image
+        return(private$proj_image)
       }
-      private$proj_image <- private$check_image(self$image)
-      return(private$proj_image)
+
     },
 
     #' @description
@@ -130,7 +140,7 @@ containr <- R6::R6Class("containr",
     #' @description
     #' `print(containr)` or `containr$print()` shows some information about the
     #' process on the screen, whether it is running and it's process id, etc.
-    print = function(){
+    proc = function(){
       cat(paste(" |", cli::style_bold("Status:"),
         if(isTRUE(self$status() == "Running")) {
           cli::col_green("Running")
@@ -181,38 +191,38 @@ containr <- R6::R6Class("containr",
     },
 
     # Image check in register or rocker values
-    check_image = function(proj_image){
-
-      proj_image <- proj_image
-      # Tag set internal for now
-      tag = NULL
-      if(is.null(tag)){
-        tag <- "latest"
-      } else {
-        tag <- tag
-      }
-
-      repo_images <- docker_images() |> dplyr::filter(Repository %in% proj_image)
-
-      data_rocker_table <- data_rocker_table
-
-      if(proj_image %in% repo_images$Repository) {
-
-        private$proj_image <- proj_image
-
-      } else if(proj_image %in% data_rocker_table$name) {
-        # Switch for changing rocker image based on image name
-        private$proj_image <- switch(proj_image,
-          rstudio = paste0("rocker/rstudio:", tag),
-          tidyerse = paste0("rocker/tidyverse:", tag),
-          verse = paste0("rocker/verse:", tag),
-          geospatial = paste0("rocker/geospatial:", tag),
-          binder = paste0("rocker/binder:", tag))
-      } else {
-        cli::cli_abort("{.var {image}} not a Rocker image or in Docker Register. Run {.fun docker_build}")
-      }
-      return(private$proj_image)
-    },
+    # check_image = function(proj_image){
+    #
+    #   proj_image <- proj_image
+    #   # Tag set internal for now
+    #   tag = NULL
+    #   if(is.null(tag)){
+    #     tag <- "latest"
+    #   } else {
+    #     tag <- tag
+    #   }
+    #
+    #   repo_images <- docker_images() |> dplyr::filter(Repository %in% proj_image)
+    #
+    #   data_rocker_table <- data_rocker_table
+    #
+    #   if(proj_image %in% repo_images$Repository) {
+    #
+    #     private$proj_image <- proj_image
+    #
+    #   } else if(proj_image %in% data_rocker_table$name) {
+    #     # Switch for changing rocker image based on image name
+    #     private$proj_image <- switch(proj_image,
+    #       rstudio = paste0("rocker/rstudio:", tag),
+    #       tidyerse = paste0("rocker/tidyverse:", tag),
+    #       verse = paste0("rocker/verse:", tag),
+    #       geospatial = paste0("rocker/geospatial:", tag),
+    #       binder = paste0("rocker/binder:", tag))
+    #   } else {
+    #     cli::cli_abort("{.var {image}} not a Rocker image or in Docker Register. Run {.fun docker_build}")
+    #   }
+    #   return(private$proj_image)
+    # },
 
     # Setup the docker command string
     setup_command = function(proj_image = NULL, proj_name = NULL, DISABLE_AUTH = NULL, use_local = NULL){
@@ -367,285 +377,11 @@ create_config_file <- function(proj_name){
 }
 
 
-# Docker Setup ------------------------------------------------------------
-
-create_dockerfile <- function(dockerfile = "docker/Dockerfile", rocker_name = "rstudio", include_python = TRUE){
-
-  cli::cli_h1("Creating Dockerfile")
-  # File overwrite warning
-  if(fs::file_exists(dockerfile)) {
-    cli::cli_alert_warning("{.emph dockerfile} will be overwritten.",
-      class = cli::cli_div(theme = list(span.emph = list(color = "orange"))))
-  }
-
-  #create_directories()
-
-  # Flag for python inclusion
-  if(isTRUE(include_python)) {
-    cli::cli_alert_info("Install Python: {.val {include_python}}")
-  } else if(isFALSE(include_python)) {
-    cli::cli_alert_info("Install Python: {.val {include_python}}")
-  }
-
-  #Set tag
-  if(is.null(tag)){
-    tag <- "latest"
-  } else {
-    tag <- tolower(tag)
-  }
-
-  rocker_name <- match.arg(rocker_name, c("rstudio", "tidyverse", "verse", "geospatial", "binder"))
-
-  rocker_image <- switch(rocker_name,
-    rstudio = paste0("rocker/rstudio:", tag),
-    tidyverse = paste0("rocker/tidyverse:", tag),
-    verse = paste0("rocker/verse:", tag),
-    geospatial = paste0("rocker/geospatial:", tag),
-    binder = paste0("rocker/binder:", tag))
-
-  cli::cli_alert_info("Using Default Image: {.val {rocker_image}}")
-
-  # command layout for dockerfile
-  docker_cmds <- paste(c(
-    paste("FROM", rocker_image),
-    # TODO paste("LABEL", "label_name"),
-    paste(""),
-    if(isTRUE(include_python)) {
-      c(
-        paste("COPY /docker/scripts/install_python.sh /tmp/"),
-        paste(""),
-        paste("COPY /docker/scripts/install_pyenv.sh /tmp/"),
-        paste(""),
-        paste("RUN", "chmod +x /tmp/install_python.sh"),
-        paste(""),
-        paste("RUN", "/tmp/install_python.sh"),
-        paste(""),
-        paste("RUN", "R -e \"reticulate::py_config()\""),
-        paste("")
-      )
-    },
-    paste("COPY /docker/scripts/install_libs_local.sh /tmp/"),
-    paste(""),
-    paste("COPY /docker/scripts/install_additional.sh /tmp/"),
-    paste(""),
-    paste("RUN chmod +x /tmp/install_libs_local.sh"),
-    paste(""),
-    paste("RUN chmod +x /tmp/install_additional.sh"),
-    paste(""),
-    paste("RUN", "/tmp/install_additional.sh")
-  ),
-    collapse = "\n")
-
-  writeLines(docker_cmds, con = dockerfile, sep = "")
-
-  if(isTRUE(file.exists(dockerfile))){
-    cli::cli_alert_success("Dockerfile saved to: {.path {dockerfile}}")
-  }
-
-}
-
-
-setup_packages <- function(packages){
-
-  bash_file <- "docker/scripts/install_libs_local.sh"
-
-  set_package_preference <- match.arg(packages, c("loaded", "none", "installed"))
-
-  if(set_package_preference %in% "none"){
-    reset_dir()
-    create_directories()
-    return(bash_file)
-  } else if(set_package_preference %in% c("loaded", "installed")){
-
-    cli::cli_h1("Setting Package Preferences")
-    create_directories()
-    package_df <- sessioninfo::package_info(pkgs = set_package_preference)
-
-    packs <- dplyr::tibble(Package = package_df$package,
-      Version = package_df$ondiskversion,
-      Source = package_df$source)
-
-    # CRAN Packages
-
-    CRAN <- packs |>
-      dplyr::filter(Source == stringr::str_match_all(Source,
-        pattern = "CRAN \\(R \\d.\\d.\\d\\)"))
-
-    # trailing slash in bash file so hack fix
-    cran_packs_first <- paste0("\t", CRAN$Package[0 -length(CRAN$Package)], " \\")
-    cran_packs_last <- paste0("\t", CRAN$Package[length(CRAN$Package) - 0], "")
-
-
-    # GitHub Packages
-    Other <- packs |>
-      dplyr::filter(Source != stringr::str_match_all(Source,
-        pattern = "CRAN \\(R \\d.\\d.\\d\\)")) |>
-      dplyr::filter(Source != "local") |>
-      dplyr::mutate(Source = stringr::str_extract_all(Source,
-        pattern = "(.*?)@",
-        simplify = TRUE)) |>
-      dplyr::mutate(Source = stringr::str_extract_all(Source,
-        pattern = "\\(([\\s\\S]*)$",
-        simplify = TRUE)) |>
-      dplyr::mutate(Source = stringr::str_remove_all(Source,
-        pattern = "\\((.*?)")) |>
-      dplyr::mutate(Source = stringr::str_remove_all(Source,
-        pattern = "@"))
-
-    not_installed <- Other |>
-      dplyr::filter(Source == "")
-
-    Github <- Other |>
-      dplyr::filter(Source != "") |>
-      dplyr::select(Source) |>
-      purrr::pluck("Source")
-
-    if(nrow(not_installed) > 0){
-      cli::cli_alert_warning("Not Installing { {nrow(not_installed)}} Package{?s}")
-      cli::cli_alert_warning("{.val {not_installed$Package}}")
-    }
-
-    if(length(Github) > 0){
-      cli::cli_alert_info("Including { {nrow(Github)}} GitHub Package{?s}")
-      cli::cli_alert_info("{.val {Github}}")
-    }
-
-    if(nrow(CRAN) > 0){
-      cli::cli_alert_info("Including { {nrow(CRAN)}} GitHub Package{?s}")
-      cli::cli_alert_info("{.val {CRAN$Package}}")
-    }
-
-    # Command layout for bash script
-    installR_script <- paste(c(
-      paste("#!/bin/bash"),
-      paste(""),
-      paste("set -e"),
-      paste(""),
-      paste("NCPUS=${NCPUS:--1}"),
-      paste(""),
-      paste("CRAN=${1:-${CRAN:-\"https://cran.r-project.org\"}}"),
-      paste(""),
-      paste("ARCH=$(uname -m)"),
-      paste(""),
-      paste("export DEBIAN_FRONTEND=noninteractive"),
-      paste(""),
-      paste("UBUNTU_VERSION=$(lsb_release -sc)"),
-      paste("CRAN_SOURCE=${CRAN/\"__linux__/$UBUNTU_VERSION/\"/\"\"}"),
-      paste(""),
-      paste("if [ \"$ARCH\" = \"aarch64\" ]; then"),
-      paste("\t", "CRAN=$CRAN_SOURCE"),
-      paste("fi"),
-      paste(""),
-      if(length(Github) > 0) {
-        c(paste("Rscript -e", paste("\"remotes::install_github(\'", Github , "\')\" ", sep = "") ," "))
-      },
-      paste(""),
-      paste("install2.r --error --skipinstalled -n \"$NCPUS\" \\"),
-      paste(cran_packs_first),
-      paste(cran_packs_last),
-      paste("")
-    ),
-      collapse = "\n")
-
-    writeLines(installR_script, con = bash_file, sep = "")
-
-    if(length(readLines(bash_file)) > 0){
-      fs::dir_tree("docker")
-      cli::cli_alert_success("Package Installs file updated: {.path {bash_file}}")
-    }
-  }
-}
-
-# Setup the docker files and folders
-create_directories = function(){
-
-  cli::cli_h2("Creating Docker Folders")
-
-  if(!fs::dir_exists("docker")) {
-    fs::dir_create("docker")
-  }
-
-  if(!fs::dir_exists("docker/scripts")) {
-    fs::dir_create("docker/scripts")
-  }
-
-  if(!fs::file_exists("docker/scripts/install_libs_local.sh")) {
-    fs::file_create("docker/scripts/install_libs_local.sh")
-  }
-
-  if(!fs::file_exists("docker/Dockerfile")) {
-    fs::file_create("docker/Dockerfile")
-  }
-
-  if(!fs::file_exists("docker/scripts/install_python.sh")) {
-    get_extdata("install_python.sh")
-  }
-
-  if(!fs::file_exists("docker/scripts/install_pyenv.sh")) {
-    get_extdata("install_pyenv.sh")
-  }
-
-  if(!fs::file_exists("docker/scripts/install_additional.sh")) {
-    get_extdata("install_additional.sh")
-  }
-
-}
-
-# Grab the bash scripts from extdata
-get_extdata = function(file){
-  if(fs::dir_exists("docker/scripts")){
-    x <- system.file("extdata",
-      file,
-      package="ContainR",
-      mustWork = TRUE)
-    fs::file_copy(x,
-      "docker/scripts",
-      overwrite = TRUE)
-
-  } else {
-    warning("docker/scripts doesnt exist")
-  }
-}
-
-reset_dir <- function(){
-  cli::cli_h2("Resetting Docker Folder")
-  if(fs::dir_exists("docker")) {
-    fs::dir_delete("docker")
-  }
-}
-
-build_image <- function(image_name = "image_name", dockerfile = "docker/Dockerfile"){
-
-  if(isFALSE(fs::file_exists(dockerfile))){
-    cli::cli_abort("No Dockerfile could be found")
-  }
-
-  if(is.null(image_name)) {
-    name_set <- tolower(basename(rstudioapi::getActiveProject()))
-  } else {
-    name_set <- tolower(name)
-  }
-
-  processx::run(command = "docker",
-    args = c("build",
-      "--no-cache=true",
-      "-t",
-      name_set,
-      "-f",
-      dockerfile,
-      "."),
-    echo_cmd = TRUE,
-    echo = TRUE,
-    spinner = TRUE)
-
-  cli::cli_alert_success("Success!")
-
-}
-
-
 # Dockerfile Class --------------------------------------------------------
 
-dockerfile <- R6::R6Class(classname = "dockerfile",
+dockerfile <- R6::R6Class(
+  classname = "dockerfile",
+  inherit = containr,
 
   # Public List -------------------------------------------------------------
 
@@ -659,7 +395,7 @@ dockerfile <- R6::R6Class(classname = "dockerfile",
     build = NULL,
     tag = NULL,
 
-    initialize = function(image_name = NA, rocker_image = NA, tag = NA, dockerfile = NA,
+    initialize = function(image_name = NA, rocker_image = NA, tag = NULL, dockerfile = NA,
       packages = NA, include_python = FALSE, build = FALSE){
 
       # These are initialized in order from top to bottom
@@ -669,10 +405,11 @@ dockerfile <- R6::R6Class(classname = "dockerfile",
       self$set_dockerfile(dockerfile)
       self$set_packages(packages)
       self$set_python(include_python)
-      self$build = build
       private$create_directories()
       private$setup_packages()
       private$create_dockerfile()
+      self$build = build
+      super$initialize(image = private$containr_image_name)
     },
 
     set_image_name = function(image_name){
@@ -691,6 +428,7 @@ dockerfile <- R6::R6Class(classname = "dockerfile",
       } else {
         self$tag <- tolower(tag)
       }
+      super$tag <- self$tag
       return(self$tag)
     },
 
